8ef3e2dc354431717e5006e93d2f4bb9cd8904ed
Lyr <lyr-7d1h@pm.me>
2024-04-04 02:59:06 +0200

    using browser history to go back and forth between code changes

--- genart-editor/src/error.ts
+++ genart-editor/src/error.ts
@@ -5,3 +5,11 @@ export function error(error: Error | string) {
     console.error(error)
   }
 }
+
+export function warn(error: Error | string) {
+  if (typeof error === 'string') {
+    console.warn(new Error(error))
+  } else {
+    console.warn(error)
+  }
+}
