c5b112ccb60962c43771bf6368097adafa8e20ed
Lyr <lyr-7d1h@pm.me>
2024-04-04 02:11:00 +0200

    added id

---
+++ genart-editor/src/id.ts
@@ -0,0 +1,58 @@
+import { GENART_EDITOR_VERSION, GENART_VERSION } from '../constants'
+
+export interface ID {
+  hash: string
+  libVersion: string
+  editorVersion: string
+  date: Date
+}
+
+export async function createID(code: string): Promise<ID> {
+  const data = new TextEncoder().encode(code)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+  // convert hash buffer to hexidecimal string
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hash = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+
+  return {
+    hash,
+    libVersion: GENART_VERSION,
+    editorVersion: GENART_EDITOR_VERSION,
+    date: new Date(),
+  }
+}
+
+function tohex(string: string) {
+  const out = []
+  for (let n = 0, l = string.length; n < l; n++) {
+    out.push(Number(string.charCodeAt(n)).toString(16))
+  }
+  return out.join('')
+}
+
+function fromhex(string: string) {
+  let out = ''
+  for (let i = 0; i < string.length; i += 2) {
+    out += String.fromCharCode(parseInt(string.substring(i, i + 2), 16))
+  }
+  return out
+}
+
+export function IDToString(id: ID): string {
+  return (
+    id.hash +
+    tohex(id.libVersion + ':' + id.editorVersion + ':' + id.date.valueOf())
+  )
+}
+
+export function IDFromString(id: string): ID {
+  const hash = id.substring(0, 64)
+  const ext = fromhex(id.substring(64)).split(':')
+
+  return {
+    hash,
+    libVersion: ext[0]!,
+    editorVersion: ext[1]!,
+    date: new Date(Number(ext[2]!)),
+  }
+}
