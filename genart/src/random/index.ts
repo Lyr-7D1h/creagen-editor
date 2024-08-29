68b3e333ad06385c30948ad73f0a31c4f26f1845
Lyr <lyr-7d1h@pm.me>
2024-08-30 00:37:07 +0200

    fix xorshift prng

--- genart/src/random/index.ts
+++ genart/src/random/index.ts
@@ -3,32 +3,3 @@ export * from './perlin'
 export * from './numgen'
 export * from './pmf'
 export * from './cdf'
-
-/**
- * A random() function, must return a number in the interval [0,1), just like Math.random().
- */
-export type RandomFn = () => number
-
-export function rng(randomFn?: RandomFn) {
-  return new RandomNumberGenerator(randomFn ?? Math.random)
-}
-
-export class RandomNumberGenerator {
-  private readonly randomFn: RandomFn
-  constructor(random: RandomFn) {
-    this.randomFn = random
-  }
-
-  integer(start: number): number
-  integer(start: number, stop: number): number
-  integer(x1: number, x2?: number) {
-    if (typeof x2 === 'undefined') {
-      return Math.round(this.randomFn() * x1)
-    }
-    return x1 + Math.round(this.randomFn() * (x2 - x1))
-  }
-
-  random() {
-    return this.randomFn()
-  }
-}
