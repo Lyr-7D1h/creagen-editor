59dad2c357319f16fcc8de0b4d4d0302ea1b2fff
Lyr <lyr-7d1h@pm.me>
2025-02-01 16:54:08 +0100

    Seperated editor

--- creagen-editor/src/env.ts
+++
@@ -1,5 +0,0 @@
-const env = import.meta.env
-export const MODE = env.MODE as 'dev' | 'release'
-export const DEBUG = env.VITE_DEBUG
-export const CREAGEN_EDITOR_VERSION = env.VITE_CREAGEN_EDITOR_VERSION
-export const CREAGEN_VERSION = env.VITE_CREAGEN_VERSION
