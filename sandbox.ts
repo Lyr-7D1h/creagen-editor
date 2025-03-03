83170bd242e7395738ef336a0524031e6f08bd50
Lyr <lyr-7d1h@pm.me>
2025-03-03 03:59:12 +0100

    Sandbox compilation

--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,27 @@
 // This file is used to run code in a sandboxed environment
 
+// import type {SandboxEvent } from './src/components/Sandbox'
 import {
   AnalyzeContainerResult,
   SandboxEvent,
   SvgProps,
 } from './src/components/Sandbox'
+import { Library } from './src/SettingsProvider'
+
+declare global {
+  interface Window {
+    creagen: {
+      constants: {
+        creagenEditorVersion: string
+      }
+    }
+  }
+}
+window.creagen = {
+  constants: {
+    creagenEditorVersion: '',
+  },
+}
 
 function postMessage(event: SandboxEvent) {
   // filter out all functions and unclonable objects (like DOM elements)
@@ -12,6 +29,7 @@ function postMessage(event: SandboxEvent) {
   parent.postMessage(serializedEvent, '*')
 }
 
+// send console logs to the parent window
 try {
   window.console = {
     ...window.console,
@@ -22,9 +40,6 @@ try {
     error: (...args) =>
       postMessage({ type: 'log', level: 'error', data: args }),
   }
-  document.addEventListener('DOMContentLoaded', () => {
-    postMessage({ type: 'loaded' })
-  })
 } catch (error) {
   postMessage({ type: 'error', error: error as Error })
 }
@@ -81,9 +96,104 @@ function analyzeSvg(html: Element): SvgProps {
 
   return { paths, circles, rects }
 }
+
 window.onload = () => {
+  postMessage({ type: 'loaded' })
   setTimeout(() => {
-    const result = analyzeContainer(document.body)
-    postMessage({ type: 'analysis', result })
+    const analysisResult = analyzeContainer(document.body)
+    postMessage({ type: 'analysisResult', analysisResult })
   }, 500)
+}
+
+window.addEventListener('message', (event) => {
+  if (event.origin !== window.origin) {
+    throw Error(`Invalid origin ${event.origin} should be ${window.origin}`)
+  }
+  const sandboxEvent = event.data
+  if (isSandboxEvent(sandboxEvent)) {
+    switch (sandboxEvent.type) {
+      case 'init':
+        window.creagen.constants = sandboxEvent.constants
+        break
+      case 'svgExportRequest':
+        const svg = document.querySelector('svg')
+        if (!svg) {
+          postMessage({ type: 'svgExportResponse', data: null })
+          return
+        }
+        exportSvg(svg, {
+          libraries: sandboxEvent.libraries,
+          name: 'asdf',
+          optimize: sandboxEvent.optimize,
+        })
+        postMessage({ type: 'svgExportResponse', data: null })
+        break
+    }
+  }
+})
+
+function isSandboxEvent(event: any): event is SandboxEvent {
+  return event && typeof event.type === 'string'
+}
+
+function exportSvg(
+  svg: SVGElement,
+  opts: { optimize: boolean; name: string; libraries: Library[] },
+) {
+  svg = svg.cloneNode(true) as SVGElement
+
+  if (opts.optimize) {
+    optimizeSvg(svg)
+  }
+
+  // TODO: add params used, code hash, date generated
+  const metadata = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'metadata',
+  )
+  const creagen = document.createElement('creagen')
+  creagen.setAttribute(
+    'libraries',
+    opts.libraries.map((l) => `${l.name}@${l.version}`).join(','),
+  )
+  creagen.setAttribute(
+    'editor-version',
+    window.creagen.constants.creagenEditorVersion,
+  )
+  creagen.setAttribute('date-generated', new Date().toISOString())
+
+  metadata.appendChild(creagen)
+  svg.appendChild(metadata)
+
+  const htmlStr = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>${svg.outerHTML}`
+  const blob = new Blob([htmlStr], { type: 'image/svg+xml' })
+
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.setAttribute('download', `${opts.name}.svg`)
+  a.setAttribute('href', url)
+  a.style.display = 'none'
+  document.body.appendChild(a)
+  a.click()
+  a.remove()
+  URL.revokeObjectURL(url)
+}
+
+function optimizeSvg(html: Element) {
+  for (const c of Array.from(html.children)) {
+    switch (c.tagName.toLocaleLowerCase()) {
+      case 'path':
+        // TODO: fix for http://localhost:5173/af438744df3a711f006203aaa39cd24e157f0f16f59ddfd6c93ea8ba00624032302e302e313a313733363532363839323337353a5b2267656e61727440302e302e35225d
+        // if (optimizePath(c as SVGPathElement, opts)) {
+        //   c.remove()
+        // }
+        break
+      case 'circle':
+        break
+      case 'rect':
+        break
+    }
+
+    optimizeSvg(c)
+  }
 }
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,66 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
-import AutoImport from '@kareemkermad/monaco-auto-import'
+import React, { useEffect, useRef } from 'react'
 import { editor } from 'monaco-editor'
 import MonacoEditor, { Monaco } from '@monaco-editor/react'
-import { initVimMode } from 'monaco-vim'
 import { useSettings } from '../../SettingsProvider'
-import { loader } from '@monaco-editor/react'
 import { Editor } from './editor'
-import * as monaco from 'monaco-editor'
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
-
-// needed for vite: https://www.npmjs.com/package/@monaco-editor/react#loader-config
-self.MonacoEnvironment = {
-  getWorker(_, label) {
-    if (label === 'json') {
-      return new jsonWorker()
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker()
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker()
-    }
-    return new editorWorker()
-  },
-}
-
-export const typescriptCompilerOptions = {
-  target: monaco.languages.typescript.ScriptTarget.ESNext,
-  allowNonTsExtensions: true,
-  moduleResolution: monaco.languages.typescript.ModuleResolutionKind.Classic,
-  esModuleInterop: true,
-  module: monaco.languages.typescript.ModuleKind.ESNext,
-  noEmit: true,
-}
-
-// Use monaco without cdn: https://www.npmjs.com/package/@monaco-editor/react#loader-config
-loader.config({ monaco })
-
-const creagenLightTheme: monaco.editor.IStandaloneThemeData = {
-  base: 'vs',
-  inherit: true,
-  colors: {},
-  rules: [],
-}
-const creagenFullscreenTheme: monaco.editor.IStandaloneThemeData = {
-  base: 'hc-black',
-  inherit: true,
-  colors: {
-    'editor.background': '#ffffff00',
-    'editor.selectionBackground': '#ffffff',
-    'editor.lineHighlightBackground': '#00000088',
-    'editorCursor.foreground': '#ffffff',
-  },
-  rules: [],
-}
 
 export interface EditorProps {
   value?: string
@@ -96,20 +38,6 @@ export function EditorView({ value, width, height, onLoad }: EditorProps) {
     editorRef,
   ])
 
-  function handleBeforeMount(monaco: Monaco) {
-    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-      noSemanticValidation: false,
-      noSyntaxValidation: false,
-      // 1378,1375: allow await on top level
-      diagnosticCodesToIgnore: [1375, 1378],
-    })
-    monaco.languages.typescript.typescriptDefaults.setCompilerOptions(
-      typescriptCompilerOptions,
-    )
-    monaco.editor.defineTheme('creagen', creagenLightTheme)
-    monaco.editor.defineTheme('creagen-fullscreen', creagenFullscreenTheme)
-  }
-
   function handleEditorDidMount(
     monacoEditor: editor.IStandaloneCodeEditor,
     monaco: Monaco,
@@ -129,7 +57,7 @@ export function EditorView({ value, width, height, onLoad }: EditorProps) {
         defaultValue={value ?? ''}
         language="typescript"
         theme="creagen"
-        beforeMount={handleBeforeMount}
+        beforeMount={Editor.handleBeforeMount}
         onMount={handleEditorDidMount}
         loading={<div>Loading...</div>}
         value=""
