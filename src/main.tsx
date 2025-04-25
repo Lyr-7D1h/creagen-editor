7eda2d8695c68740409a5545171b1699c3e3be51
Lyr <lyr-7d1h@pm.me>
2025-04-26 01:07:16 +0200

    remove unused

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { IndexDB } from './storage/storage'
 import { CreagenEditorView } from './creagen-editor/CreagenEditorView'
 
 const root = createRoot(document.getElementById('root')!)
-
-const indexdb = new IndexDB()
 
 root.render(<CreagenEditorView />)
