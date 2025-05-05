62252fc65d9880cf4483ac5fb24878a11d43c36a
Lyr <lyr-7d1h@pm.me>
2025-05-05 15:51:26 +0200

    Loading bar to logs

--- src/logs/Logs.tsx
+++ src/logs/Logs.tsx
@@ -1,6 +1,6 @@
-import { Alert, AlertColor, Stack } from '@mui/material'
+import { Alert, AlertColor, Stack, CircularProgress } from '@mui/material'
 import React, { JSX, useEffect, useState } from 'react'
-import { Message, Severity, logger } from './logger'
+import { AUTOHIDE_DURATION_DEFAULT, Message, Severity, logger } from './logger'
 
 function severityToAlertColor(severity: Severity): AlertColor {
   switch (severity) {
@@ -19,15 +19,60 @@ function severityToAlertColor(severity: Severity): AlertColor {
 
 export function Logs() {
   const [logs, setLogs] = useState<Message[]>(logger.getMessages())
+  const [timeLeft, setTimeLeft] = useState<Record<string, number>>({})
+  const updateInterval = 50
 
   useEffect(() => {
     const unsubscribe = logger.subscribe((alerts) => {
       setLogs(alerts)
+
+      // add new logs to timeLeft
+      setTimeLeft((prev) => {
+        const updated = { ...prev }
+        Object.values(alerts).forEach((msg) => {
+          const id = msg.id
+          if (
+            typeof updated[id] === 'undefined' &&
+            msg.autoHideDuration !== null
+          ) {
+            updated[id] = msg.autoHideDuration ?? AUTOHIDE_DURATION_DEFAULT
+          }
+        })
+        return updated
+      })
     })
+
     return () => {
       unsubscribe()
     }
   }, [])
+
+  useEffect(() => {
+    if (logs.length === 0) return
+
+    const interval = setInterval(() => {
+      setTimeLeft((prev) => {
+        const updated = { ...prev }
+
+        Object.keys(updated).forEach((id) => {
+          const log = logs.find((log) => log.id === id)
+          // remove log if it doesn't exist anymore
+          if (typeof log === 'undefined') {
+            delete updated[id]
+            return
+          }
+
+          const duration = log.autoHideDuration ?? AUTOHIDE_DURATION_DEFAULT
+          const decrementValue = 100 / (duration / updateInterval)
+          updated[id] = Math.max(0, updated[id]! - decrementValue)
+        })
+
+        return updated
+      })
+    }, updateInterval)
+
+    return () => clearInterval(interval)
+  }, [logs])
 
   const handleClose = (id: string) => {
     logger.remove(id)
@@ -45,8 +90,19 @@ export function Logs() {
       <Alert
         key={log.id}
         severity={severityToAlertColor(severity)}
-        onClose={() => handleClose(log.id)}
         sx={{ minWidth: '250px' }}
+        onClose={() => handleClose(log.id)}
+        icon={
+          log.autoHideDuration !== null && (
+            <CircularProgress
+              variant="determinate"
+              value={100 - (timeLeft[log.id] || 0)}
+              size={20}
+              thickness={5}
+              sx={{ mr: 1, color: 'rgba(0, 0, 0, 0.3)' }}
+            />
+          )
+        }
       >
         {log.message}
       </Alert>,
