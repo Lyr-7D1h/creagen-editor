df8a9035ea4c228a8ac280c1032a55165965f829
Lyr <lyr-7d1h@pm.me>
2026-08-19 17:04:33 +0200

    fix: Fix dirname import

--- vite.lib.config.mjs
+++ vite.lib.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: path.resolve(import.meta.dirname, 'src/index.ts'),
       name: 'CreagenEditor',
       formats: ['es', 'cjs'],
       fileName: (format) =>
